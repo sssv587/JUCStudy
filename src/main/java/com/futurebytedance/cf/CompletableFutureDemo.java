7f2e8ef048914e4065c2c32e23f187e4e5858b37
sssv587 <1092663295@qq.com>
2022-11-05 16:41:24 +0800

    CompletableFuture引入FutureTask

--- src/main/java/com/futurebytedance/cf/CompletableFutureDemo.java
+++ src/main/java/com/futurebytedance/cf/CompletableFutureDemo.java
@@ -1,6 +1,8 @@
 package com.futurebytedance.cf;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 /**
  * @author yuhang.sun
@@ -9,23 +11,31 @@ import java.util.concurrent.Callable;
  * @Description FutureTask
  */
 public class CompletableFutureDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask<String> futureTask = new FutureTask<String>(new MyThread());
 
+        //因为FutureTask实现了Runnable接口
+        Thread t1 = new Thread(futureTask, "t1");
+        t1.start();
+
+        //获取Callable接口的返回值
+        System.out.println(futureTask.get());
     }
 }
 
-class MyThread implements Runnable{
+//class MyThread implements Runnable{
+//
+//    @Override
+//    public void run() {
+//
+//    }
+//}
 
-    @Override
-    public void run() {
-
-    }
-}
-
-class MyThread2 implements Callable<String>{
+class MyThread implements Callable<String>{
 
     @Override
     public String call() throws Exception {
-        return null;
+        System.out.println("------come in call()");
+        return "hello Callable";
     }
 }

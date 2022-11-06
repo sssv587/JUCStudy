49b41238e4f822327cdbcef95fa6c48e7c5b5dd7
sssv587 <1092663295@qq.com>
2022-11-06 23:28:12 +0800

    CompletableFuture之电商比价案例实战

--- src/main/java/com/futurebytedance/cf/CompletableFutureMall.java
+++ src/main/java/com/futurebytedance/cf/CompletableFutureMall.java
@@ -2,27 +2,79 @@ package com.futurebytedance.cf;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * @author yuhang.sun
  * @version 1.0
  * @date 2022/11/6 - 22:58
- * @Description
+ * @Description CompletableFuture实战
+ * 案例说明：电商比价需求，模拟如下情况：
+ * <p>
+ * 1需求：
+ * 1.1 同一款产品，同时搜索出同款产品在各大电商平台的售价;
+ * 1.2 同一款产品，同时搜索出本产品在同一个电商平台下，各个入驻卖家售价是多少
+ * <p>
+ * 2输出：出来结果希望是同款产品的在不同地方的价格清单列表，返回一个List<String>
+ * 《mysql》 in jd price is 88.05
+ * 《mysql》 in dangdang price is 86.11
+ * 《mysql》 in taobao price is 90.43
+ * <p>
+ * 3 技术要求
+ * 3.1 函数式编程
+ * 3.2 链式编程
+ * 3.3 Stream流式计算
  */
 public class CompletableFutureMall {
+    static List<NetMall> list = Arrays.asList(
+            new NetMall("jd"),
+            new NetMall("dangdang"),
+            new NetMall("taobao")
+    );
+
+    /**
+     * step by step 一家家搜
+     *
+     * @param list
+     * @param productName
+     * @return
+     */
+    public static List<String> getPrice(List<NetMall> list, String productName) {
+        //《mysql》 in jd price is 88.05
+        return list
+                .stream()
+                .map(netMall -> String.format(productName + " in %s price is %.2f", netMall.getNetMallName(), netMall.calcPrice(productName)))
+                .collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
-        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
-            return "hello 1234";
-        });
+        long startTime = System.currentTimeMillis();
+
+        List<String> list1 = getPrice(list, "mysql");
+        for (String element : list1) {
+            System.out.println(element);
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("---costTime:" + (endTime - startTime) + " 毫秒");
+
+//        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+//            return "hello 1234";
+//        });
 
 //        System.out.println(completableFuture.get());
         //join()和get()作用几乎一样,就是在编译的时候不报错
-        System.out.println(completableFuture.join());
+//        System.out.println(completableFuture.join());
 
 
 //        Student student = new Student();
@@ -31,6 +83,24 @@ public class CompletableFutureMall {
 //        student.setMajor("cs");
 //
 //        student.setId(12).setStudentName("lisi").setMajor("english");
+    }
+}
+
+class NetMall {
+    @Getter
+    private String netMallName;
+
+    public NetMall(String netMallName) {
+        this.netMallName = netMallName;
+    }
+
+    public double calcPrice(String productName) {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ThreadLocalRandom.current().nextDouble() * 2 + productName.charAt(0);
     }
 }
 

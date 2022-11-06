74d20e44b9def128de75fb534eefccad2a0dbc09
sssv587 <1092663295@qq.com>
2022-11-06 22:49:00 +0800

    CompletableFuture之通用异步编程

--- src/main/java/com/futurebytedance/cf/CompletableFutureUseDemo.java
+++ src/main/java/com/futurebytedance/cf/CompletableFutureUseDemo.java
@@ -1,9 +1,6 @@
 package com.futurebytedance.cf;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author yuhang.sun
@@ -13,25 +10,36 @@ import java.util.concurrent.TimeUnit;
  */
 public class CompletableFutureUseDemo {
     public static void main(String[] args) {
-        CompletableFuture.supplyAsync(() -> {
-            System.out.println(Thread.currentThread().getName() + "-----come in");
-            int result = ThreadLocalRandom.current().nextInt(10);
-            try {
-                TimeUnit.MILLISECONDS.sleep(1000);
-            } catch (Exception e) {
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+
+        try {
+            CompletableFuture.supplyAsync(() -> {
+                System.out.println(Thread.currentThread().getName() + "-----come in");
+                int result = ThreadLocalRandom.current().nextInt(10);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                System.out.println("-----1秒钟后出结果:" + result);
+                if (result > 5) {
+                    int i = 10 / 0;
+                }
+                return result;
+            }, threadPool).whenComplete((v, e) -> {
+                if (e == null) {
+                    System.out.println("-----------计算完成,更新系统UpdateValue:" + v);
+                }
+            }).exceptionally(e -> {
                 e.printStackTrace();
-            }
-            System.out.println("-----1秒钟后出结果:" + result);
-            return result;
-        }).whenComplete((v, e) -> {
-            if (e == null) {
-                System.out.println("-----------计算完成,更新系统UpdateValue:" + v);
-            }
-        }).exceptionally(e -> {
+                System.out.println("异常情况:" + e.getCause() + "\t" + e.getMessage());
+                return null;
+            });
+        } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("异常情况:" + e.getCause() + "\t" + e.getMessage());
-            return null;
-        });
+        } finally {
+            threadPool.shutdown();
+        }
 
         System.out.println(Thread.currentThread().getName() + "线程先去忙其他任务");
 

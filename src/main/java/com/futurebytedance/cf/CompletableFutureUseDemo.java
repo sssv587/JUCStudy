16ec086c56e4286a1d18d5ae8338e3b9c218584f
sssv587 <1092663295@qq.com>
2022-11-06 22:42:04 +0800

    CompletableFuture之通用异步编程

---
+++ src/main/java/com/futurebytedance/cf/CompletableFutureUseDemo.java
@@ -0,0 +1,65 @@
+package com.futurebytedance.cf;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/11/6 - 22:26
+ * @Description CompletableFutureUse的使用
+ */
+public class CompletableFutureUseDemo {
+    public static void main(String[] args) {
+        CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + "-----come in");
+            int result = ThreadLocalRandom.current().nextInt(10);
+            try {
+                TimeUnit.MILLISECONDS.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("-----1秒钟后出结果:" + result);
+            return result;
+        }).whenComplete((v, e) -> {
+            if (e == null) {
+                System.out.println("-----------计算完成,更新系统UpdateValue:" + v);
+            }
+        }).exceptionally(e -> {
+            e.printStackTrace();
+            System.out.println("异常情况:" + e.getCause() + "\t" + e.getMessage());
+            return null;
+        });
+
+        System.out.println(Thread.currentThread().getName() + "线程先去忙其他任务");
+
+        //ForkJoinPool感觉很像一个守护线程,如果主线程执行完了,会直接关闭任务
+        //主线程不要立刻结束,否则CompletableFuture默认使用的线程池会立刻关闭:暂停3秒钟线程
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(3000);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    //完全可以支持future的功能
+    private static void future1() throws ExecutionException, InterruptedException {
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + "-----come in");
+            int result = ThreadLocalRandom.current().nextInt(10);
+            try {
+                TimeUnit.MILLISECONDS.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("-----1秒钟后出结果:" + result);
+            return result;
+        });
+
+        System.out.println(Thread.currentThread().getName() + "线程先去忙其他任务");
+
+        System.out.println(completableFuture.get());
+    }
+}

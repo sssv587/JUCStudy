79500135a1a0cf469a48594f2b69b277face0a21
sssv587 <1092663295@qq.com>
2022-11-10 00:11:30 +0800

    中断机制之通过interrupt实现线程中断停止

--- src/main/java/com/futurebytedance/interrupt/InterruptDemo.java
+++ src/main/java/com/futurebytedance/interrupt/InterruptDemo.java
@@ -10,7 +10,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * @Description 如何停止中断运行中的线程?
  *  1.通过一个volatile变量实现
  *  2.通过AtomicBoolean
- *
+ *  3.通过Thread类自带的中断api实例方法实现
+ *      在需要中断的线程中不断监听中断状态,一旦发生中断,就执行相应的中断处理业务逻辑stop线程
  */
 public class InterruptDemo {
     static volatile boolean isStop = false;
@@ -20,6 +21,36 @@ public class InterruptDemo {
 //        m1_volatile();
 
 //        m2_atomicBoolean();
+
+        m3_thread();
+    }
+
+    //3.通过Thread类自带的中断api实例方法实现
+    private static void m3_thread() {
+        Thread t1 = new Thread(() -> {
+            while (true) {
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println(Thread.currentThread().getName() + "\t isInterrupted()被修改为true,程序停止");
+                    break;
+                }
+                System.out.println("t1 .....hello interrupt api");
+            }
+        }, "t1");
+        t1.start();
+
+        System.out.println("-------t1的默认中断标识位:" + t1.isInterrupted());
+
+        //暂停毫秒
+        try {
+            TimeUnit.MILLISECONDS.sleep(20);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //t2向t1发出协商,将t1的中断标志位设为true希望t1停下来
+        new Thread(t1::interrupt, "t2").start();
+        //也可以自己设置
+//        t1.interrupt();
     }
 
     //2.通过AtomicBoolean

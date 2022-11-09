3e0c10b22b4f6bf6c33c18d3b9647fce28b031f1
sssv587 <1092663295@qq.com>
2022-11-09 23:35:05 +0800

    中断机制之中断协商机制简介

---
+++ src/main/java/com/futurebytedance/interrupt/InterruptDemo.java
@@ -0,0 +1,14 @@
+package com.futurebytedance.interrupt;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/11/9 - 23:28
+ * @Description 中断协商机制
+ */
+public class InterruptDemo {
+    public static void main(String[] args) {
+        //kill stop......
+
+    }
+}

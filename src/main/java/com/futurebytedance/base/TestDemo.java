1477097fcdc5f6999cc200c2f8a10a5c3608e5c5
sssv587 <1092663295@qq.com>
2022-11-13 23:26:43 +0800

    JMM三大特性

---
+++ src/main/java/com/futurebytedance/base/TestDemo.java
@@ -0,0 +1,17 @@
+package com.futurebytedance.base;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/11/13 - 23:10
+ * @Description
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Dog {
+    int age;
+}

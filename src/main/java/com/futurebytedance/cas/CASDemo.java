5eb8da74dbb1dbcd07682bdeb824f6bb20c839c4
sssv587 <1092663295@qq.com>
2022-11-19 22:53:56 +0800

    CAS之源码分析引出Unsafe类

---
+++ src/main/java/com/futurebytedance/cas/CASDemo.java
@@ -0,0 +1,18 @@
+package com.futurebytedance.cas;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/11/19 - 22:46
+ * @Description CAS
+ */
+public class CASDemo {
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger = new AtomicInteger(5);
+
+        System.out.println(atomicInteger.compareAndSet(5, 2022) + "\t" + atomicInteger.get());
+        System.out.println(atomicInteger.compareAndSet(5, 2022) + "\t" + atomicInteger.get());
+    }
+}

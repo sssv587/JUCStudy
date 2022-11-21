72f15e13931288caf60257df77dc8d61cf754690
sssv587 <1092663295@qq.com>
2022-11-22 00:09:25 +0800

    ThreadLocal之弱引用

--- src/main/java/com/futurebytedance/threadlocal/ReferenceDemo.java
+++ src/main/java/com/futurebytedance/threadlocal/ReferenceDemo.java
@@ -1,6 +1,7 @@
 package com.futurebytedance.threadlocal;
 
 import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -10,11 +11,28 @@ import java.util.concurrent.TimeUnit;
  * @Description ThreadLocal弱引用 引出问题
  * 强引用:当内存不足，JVM开始垃圾回收,对于强引用的对象,就算是出现了OOM也不会对该对象进行回收,死都不收
  * 软引用:对于只有软引用的对象来说,当系统内存充足时它 不会被回收,当系统内存不足时它会被回收
+ * 弱引用:对于只有弱引用的对象来说,只要垃圾回收机制一运行,不管JVM的内存空间是否足够,都会回收该对象占用的内存
+ *
+ * 软引用和弱引用的适用场景
+ *      假如有一个应用需要读取大量的本地图片:
+ *      如果每次读取图片都从硬盘读取则会严重影响性能,
+ *      如果一次性全部加载到内存中又可能造成内存溢出。
+ * 此时使用软引用可以解决这个问题。
+ * 设计思路是：用一个HashMap来保存图片的路径和相应图片对象关联的软引用之间的映射关系，在内存不足时，JVM会自动回收这些缓存图片对象所占用的空间，从而有效地避免了OOM的问题。
+ * Map<String, SoftReference<Bitmap>> imageCache = new HashMap<String, SoftReference<Bitmap>>();
  */
 public class ReferenceDemo {
     public static void main(String[] args) {
 //        strongReference();
-        softReference();
+//        softReference();
+        WeakReference<MyObject> weakReference = new WeakReference<>(new MyObject());
+        System.out.println("-------gc before 内存够用:" + weakReference.get());
+
+        System.gc();
+
+        try {TimeUnit.SECONDS.sleep(1);} catch (Exception e) {e.printStackTrace();}
+
+        System.out.println("-------gc before 内存够用:" + weakReference.get());
     }
 
     private static void softReference() {

a25ac1b2d0362e543711f66b996d3e70b2d39b77
sssv587 <1092663295@qq.com>
2022-11-21 23:54:45 +0800

    ThreadLocal之强引用

--- src/main/java/com/futurebytedance/threadlocal/ReferenceDemo.java
+++ src/main/java/com/futurebytedance/threadlocal/ReferenceDemo.java
@@ -1,14 +1,30 @@
 package com.futurebytedance.threadlocal;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author yuhang.sun
  * @version 1.0
  * @date 2022/11/21 - 23:45
- * @Description ThreadLocal弱引用
+ * @Description ThreadLocal弱引用 引出问题
+ * 强、软、弱、虚引用
  */
 public class ReferenceDemo {
     public static void main(String[] args) {
+//        strongReference();
+    }
 
+    //强引用
+    private static void strongReference() {
+        MyObject myObject = new MyObject();
+        System.out.println("gc before:" + myObject);
+
+        myObject = null;
+        System.gc();//人工开启GC,一般不用
+
+        try {TimeUnit.MILLISECONDS.sleep(500);} catch (Exception e) {e.printStackTrace();}
+
+        System.out.println("gc after:" + myObject);
     }
 }
 

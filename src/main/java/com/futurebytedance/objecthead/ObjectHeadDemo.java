81dd3e988371df2cc73d4bcdbac152e40023a2fb
sssv587 <1092663295@qq.com>
2022-11-22 11:59:47 +0800

    对象内存布局之布局简介

---
+++ src/main/java/com/futurebytedance/objecthead/ObjectHeadDemo.java
@@ -0,0 +1,13 @@
+package com.futurebytedance.objecthead;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/11/22 - 11:52
+ * @Description
+ */
+public class ObjectHeadDemo {
+    public static void main(String[] args) {
+        Object object = new Object();
+    }
+}

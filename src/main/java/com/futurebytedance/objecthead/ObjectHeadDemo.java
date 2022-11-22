b8c201d0e74bf605e79e571488854f7feed8c10b
sssv587 <1092663295@qq.com>
2022-11-22 22:37:13 +0800

    对象内存布局之对象标记MarkWord

--- src/main/java/com/futurebytedance/objecthead/ObjectHeadDemo.java
+++ src/main/java/com/futurebytedance/objecthead/ObjectHeadDemo.java
@@ -4,10 +4,19 @@ package com.futurebytedance.objecthead;
  * @author yuhang.sun
  * @version 1.0
  * @date 2022/11/22 - 11:52
- * @Description
+ * @Description 对象内存布局
  */
 public class ObjectHeadDemo {
     public static void main(String[] args) {
-        Object object = new Object();
+        Object o = new Object(); //new一个对象,占内存多少?
+        //在64位系统中,Mark Word占了8个字节,类型指针占了8个字节,一共是16个字节
+
+        System.out.println(o.hashCode());//这个hashcode记录在对象的什么地方
+
+        synchronized (o) {
+
+        }
+
+        System.gc();//手动手机垃圾。。。。。15次可以从新生代---养老区
     }
 }

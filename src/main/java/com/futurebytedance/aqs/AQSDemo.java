b5d49eff7ff5b39fadda492c228080300f6d41a9
sssv587 <1092663295@qq.com>
2022-11-24 00:34:23 +0800

    AQS开篇简介

---
+++ src/main/java/com/futurebytedance/aqs/AQSDemo.java
@@ -0,0 +1,22 @@
+package com.futurebytedance.aqs;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/11/24 - 0:31
+ * @Description AQS
+ */
+public class AQSDemo {
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock();
+
+        lock.lock();
+        try {
+
+        } finally {
+            lock.unlock();
+        }
+    }
+}

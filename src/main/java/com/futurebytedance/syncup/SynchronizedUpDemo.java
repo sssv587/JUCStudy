175974add3e6d805473b19c9ba6bc4f7d5056797
sssv587 <1092663295@qq.com>
2022-11-23 23:52:02 +0800

    synchronized锁升级之锁升级后和hashcode的代码证明

--- src/main/java/com/futurebytedance/syncup/SynchronizedUpDemo.java
+++ src/main/java/com/futurebytedance/syncup/SynchronizedUpDemo.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
  * @author yuhang.sun
  * @version 1.0
  * @date 2022/11/23 - 0:40
- * @Description 锁升级之无锁、偏向锁、轻量锁、重量锁
+ * @Description 锁升级之无锁、偏向锁、轻量锁、重量锁与锁升级后hashcode去哪了
  */
 public class SynchronizedUpDemo {
     static final Object o = new Object();
@@ -17,7 +17,27 @@ public class SynchronizedUpDemo {
 //        noLock();
 //        biasedLock();
 //        lightWeightLock();
-        heavyWeightLock();
+//        heavyWeightLock();
+
+        //先睡眠5秒,保证开启偏向锁
+        try {TimeUnit.MILLISECONDS.sleep(5000);} catch (Exception e) {e.printStackTrace();}
+
+//        Object o = new Object();
+//        System.out.println("本应是偏向锁:");
+//        System.out.println(ClassLayout.parseInstance(o).toPrintable());
+//
+//        o.hashCode();//没有重写,一致性哈希,重写后无效,当一个对象已经计算过identity hash code 它就无法进入偏向锁状态
+//
+//        synchronized (o){
+//            System.out.println("本应是偏向锁,但是由于计算过一致性哈希,会直接升级为轻量级锁");
+//            System.out.println(ClassLayout.parseInstance(o).toPrintable());
+//        }
+
+        synchronized (o){
+            o.hashCode();
+            System.out.println("偏向锁过程中遇到一致性哈希计算请求,立马撤销偏向模式,膨胀为重量级锁");
+            System.out.println(ClassLayout.parseInstance(o).toPrintable());
+        }
     }
 
     //重量级锁

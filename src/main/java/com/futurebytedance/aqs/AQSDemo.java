3644bbc333c8f70113ff70bd5f0c33e476f28561
sssv587 <1092663295@qq.com>
2022-11-24 23:36:18 +0800

    AQS之源码分析02

--- src/main/java/com/futurebytedance/aqs/AQSDemo.java
+++ src/main/java/com/futurebytedance/aqs/AQSDemo.java
@@ -1,6 +1,7 @@
 package com.futurebytedance.aqs;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -11,15 +12,54 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class AQSDemo {
     public static void main(String[] args) {
-        ReentrantLock lock = new ReentrantLock();
+        ReentrantLock reentrantLock = new ReentrantLock();//非公平锁
 
-        lock.lock();
-        try {
+        // A B C三个顾客，去银行办理业务，A先到，此时窗口空无一人，他优先获得办理窗口的机会，办理业务。
+        // A 耗时严重，估计长期占有窗口
+        new Thread(() -> {
+            reentrantLock.lock();
+            try
+            {
+                System.out.println("----come in A");
+                //暂停20分钟线程
+                try { TimeUnit.MINUTES.sleep(20); } catch (InterruptedException e) { e.printStackTrace(); }
+            }finally {
+                reentrantLock.unlock();
+            }
+        },"A").start();
 
-        } finally {
-            lock.unlock();
-        }
+        //B是第2个顾客，B一看到受理窗口被A占用，只能去候客区等待，进入AQS队列，等待着A办理完成，尝试去抢占受理窗口。
+        new Thread(() -> {
+            reentrantLock.lock();
+            try
+            {
+                System.out.println("----come in B");
+            }finally {
+                reentrantLock.unlock();
+            }
+        },"B").start();
 
-        new CountDownLatch(10);
+
+        //C是第3个顾客，C一看到受理窗口被A占用，只能去候客区等待，进入AQS队列，等待着A办理完成，尝试去抢占受理窗口,前面是B顾客，FIFO
+        new Thread(() -> {
+            reentrantLock.lock();
+            try
+            {
+                System.out.println("----come in C");
+            }finally {
+                reentrantLock.unlock();
+            }
+        },"C").start();
+
+        //后续顾客DEFG。。。。。。。以此类推
+        new Thread(() -> {
+            reentrantLock.lock();
+            try
+            {
+                //。。。。。。
+            }finally {
+                reentrantLock.unlock();
+            }
+        },"D").start();
     }
 }

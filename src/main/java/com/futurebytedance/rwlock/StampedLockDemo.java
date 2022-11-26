e9c8617077f0cacb8f8fca3a7e1486ac72a3b475
sssv587 <1092663295@qq.com>
2022-11-26 18:31:50 +0800

    StampedLock锁之乐观读功能

--- src/main/java/com/futurebytedance/rwlock/StampedLockDemo.java
+++ src/main/java/com/futurebytedance/rwlock/StampedLockDemo.java
@@ -14,6 +14,7 @@ public class StampedLockDemo {
     static int number = 37;
     static StampedLock stampedLock = new StampedLock();
 
+    //传统的写方法
     public void write() {
         //返回戳记
         long stamp = stampedLock.writeLock();
@@ -26,6 +27,7 @@ public class StampedLockDemo {
         System.out.println(Thread.currentThread().getName() + "\t" + "写线程结束修改");
     }
 
+    //传统的读方法
     //悲观读,读没有完成的时候写锁无法获得
     public void read() {
         long stamp = stampedLock.readLock();
@@ -47,8 +49,40 @@ public class StampedLockDemo {
         }
     }
 
+    //乐观读,读的过程也允许获取写锁介入
+    public void tryOptimisticRead() {
+        long stamp = stampedLock.tryOptimisticRead();
+        int result = number;
+        //故意间隔4秒钟，很乐观认为读取中没有其它线程修改过number值，具体靠判断
+        System.out.println("4秒前stampedLock.validate方法值(true无修改，false有修改)" + "\t" + stampedLock.validate(stamp));
+        for (int i = 0; i < 4; i++) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "\t" + "正在读取... " + (i + 1) + " 秒" +
+                    "后stampedLock.validate方法值(true无修改，false有修改)" + "\t" + stampedLock.validate(stamp));
+        }
+        if (!stampedLock.validate(stamp)) {
+            System.out.println("有人修改过------有写操作");
+            stamp = stampedLock.readLock();
+            try {
+                System.out.println("从乐观读 升级为 悲观读");
+                result = number;
+                System.out.println("重新悲观读后result：" + result);
+            } finally {
+                stampedLock.unlockRead(stamp);
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + "\t" + " finally value: " + result);
+    }
+
     public static void main(String[] args) {
         StampedLockDemo resource = new StampedLockDemo();
+
+        //传统版读写锁
+        /*
         new Thread(() -> {
             resource.read();
         }, "readThread").start();
@@ -64,8 +98,34 @@ public class StampedLockDemo {
             resource.write();
         }, "writeThread").start();
 
-        try {TimeUnit.MILLISECONDS.sleep(4000);} catch (Exception e) {e.printStackTrace();}
+        try {
+            TimeUnit.MILLISECONDS.sleep(4000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        System.out.println(Thread.currentThread().getName() + "\t" + "number:" + number);
+        System.out.println(Thread.currentThread().getName() + "\t" + "number:" + number);*/
+
+        new Thread(() -> {
+            resource.tryOptimisticRead();
+        }, "readThread").start();
+
+        //暂停2s线程,读读过程可以写介入,演示
+//        try {
+//            TimeUnit.SECONDS.sleep(2);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        //暂停2s线程,没有写线程介入
+        try {
+            TimeUnit.SECONDS.sleep(6);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t" + "-----come in");
+            resource.write();
+        }, "writeThread").start();
     }
 }

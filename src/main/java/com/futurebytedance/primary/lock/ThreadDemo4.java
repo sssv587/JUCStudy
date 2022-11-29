d843d03ce897c357bf51a4ab8a3f09e0267f867d
sssv587 <1092663295@qq.com>
2022-11-29 23:57:50 +0800

    HashSet与HashMap线程不安全问题解决

--- src/main/java/com/futurebytedance/primary/lock/ThreadDemo4.java
+++ src/main/java/com/futurebytedance/primary/lock/ThreadDemo4.java
@@ -1,7 +1,9 @@
 package com.futurebytedance.primary.lock;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * @author yuhang.sun
@@ -24,12 +26,40 @@ public class ThreadDemo4 {
         List<String> list = new CopyOnWriteArrayList<>();
 
         //java.util.ConcurrentModificationException
+//        for (int i = 0; i < 30; i++) {
+//            new Thread(() -> {
+//                //向集合添加内容
+//                list.add(UUID.randomUUID().toString().substring(0, 8));
+//                //从集合获取内容
+//                System.out.println(list);
+//            }, String.valueOf(i)).start();
+//        }
+
+        //演示HashSet java.util.ConcurrentModificationException
+//        Set<String> set = new HashSet<>();
+
+//        Set<String> set = new CopyOnWriteArraySet<>();
+//        for (int i = 0; i < 30; i++) {
+//            new Thread(() -> {
+//                //向集合添加内容
+//                set.add(UUID.randomUUID().toString().substring(0, 8));
+//                //从集合获取内容
+//                System.out.println(set);
+//            }, String.valueOf(i)).start();
+//        }
+
+        //演示HashMap java.util.ConcurrentModificationException
+//        Map<String, String> map = new HashMap<>();
+
+        Map<String, String> map = new ConcurrentHashMap<>();
+
         for (int i = 0; i < 30; i++) {
+            String key = String.valueOf(i);
             new Thread(() -> {
                 //向集合添加内容
-                list.add(UUID.randomUUID().toString().substring(0, 8));
+                map.put(key, UUID.randomUUID().toString().substring(0, 8));
                 //从集合获取内容
-                System.out.println(list);
+                System.out.println(map);
             }, String.valueOf(i)).start();
         }
     }

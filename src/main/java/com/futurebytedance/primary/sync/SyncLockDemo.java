b105b86fc2cd270b1ad77eef7726de1e1fd5c8c9
sssv587 <1092663295@qq.com>
2022-12-01 00:39:37 +0800

    Lock可重入锁

--- src/main/java/com/futurebytedance/primary/sync/SyncLockDemo.java
+++ src/main/java/com/futurebytedance/primary/sync/SyncLockDemo.java
@@ -1,5 +1,8 @@
 package com.futurebytedance.primary.sync;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author yuhang.sun
  * @version 1.0
@@ -12,7 +15,35 @@ public class SyncLockDemo {
     }
 
     public static void main(String[] args) {
-        new SyncLockDemo().add();
+        //Lock演示可重入锁
+        Lock lock = new ReentrantLock();
+        //创建线程
+        new Thread(() -> {
+            try {
+                //上锁
+                lock.lock();
+                System.out.println(Thread.currentThread().getName() + " 外层");
+                try {
+                    //上锁
+                    lock.lock();
+                    System.out.println(Thread.currentThread().getName() + " 内层");
+                } finally {
+                    lock.unlock();
+                }
+            } finally {
+                //释放锁
+                lock.unlock();
+            }
+        }, "t2").start();
+
+        //创建新线程
+        new Thread(() -> {
+            lock.lock();
+            System.out.println("aaaa");
+            lock.unlock();
+        }, "t3").start();
+
+//        new SyncLockDemo().add();
         // synchronized
         Object o = new Object();
         new Thread(() -> {
